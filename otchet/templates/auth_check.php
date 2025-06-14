524e9fb6ca2a2ebe888adc3baf48d2799a8a15ec
dodoku02 <ivanova.wlad@yandex.ru>
2025-06-14 12:02:17 +0300

    Delete otchet directory

--- otchet/templates/auth_check.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-// Проверяет, залогинился ли пользователь
-if (!isset($_SESSION['user_id'])) {
-    header('Location: login.php'); // Перенаправляет на страницу входа
-    exit();
-}
